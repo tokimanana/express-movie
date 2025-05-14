28e6467b5ef5f1b3b4577cebfea15ae7b5a75367
tokimanana <tokimananasarobidy@gmail.com>
2025-05-14 11:07:30 +0400

    GET & PUT with parse for postman requirement

--- app.js
+++ app.js
@@ -6,22 +6,14 @@ const upload = multer();
 const cors = require("cors");
 
 const jwt = require("jsonwebtoken");
-const { expressjwt: expressjwt } = require("express-jwt"); // to verify token on the request header
+const { expressjwt: expressjwt } = require("express-jwt");
 
 const faker = require("faker");
 const config = require("./config");
 
 const mongoose = require("mongoose");
 
-mongoose.connect(
-  `mongodb+srv://${config.user}:${config.password}@expressmovie.gxdzkla.mongodb.net/`
-);
-const db = mongoose.connection;
-db.on("error", console.error.bind(console, "connection error"));
-db.once("open", () => {
-  console.log("connected to the DB");
-});
-
+// First define the schema
 const movieSchema = mongoose.Schema(
   {
     movieTitle: String,
@@ -30,23 +22,37 @@ const movieSchema = mongoose.Schema(
   { versionKey: false }
 );
 
+// Then create the model
 const Movie = mongoose.model("Movie", movieSchema);
 
-const title = faker.lorem.sentence(3);
-const year = Math.floor(Math.random() * 80) + 1950;
+// Connect to MongoDB
+mongoose.connect(
+  `mongodb+srv://tokimananasarobidy:JesosyFamonjena.@expressmovie.tsqqcvd.mongodb.net/`
+);
 
-const myMovie = new Movie({
-  movieTitle: title,
-  movieYear: year,
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "MongoDB connection error:"));
+db.once("open", () => {
+  console.log("Connected to MongoDB");
+
+  // Only create and save the movie after successful connection
+  // const title = faker.lorem.sentence(3);
+  // const year = Math.floor(Math.random() * 80) + 1950;
+
+  // const myMovie = new Movie({
+  //   movieTitle: title,
+  //   movieYear: year,
+  // });
+
+  // myMovie
+  //   .save()
+  //   .then((savedMovie) => {
+  //     console.log("Saved movie:", savedMovie);
+  //   })
+  //   .catch((err) => {
+  //     console.error("Error saving movie:", err);
+  //   });
 });
-myMovie
-  .save()
-  .then((savedMovie) => {
-    console.log("Saved movie : ", savedMovie);
-  })
-  .catch((err) => {
-    console.error(err);
-  });
 
 const PORT = 3000;
 
@@ -54,6 +60,8 @@ const PORT = 3000;
 app.use("/public", express.static("public"));
 
 app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 
 const secret =
   "qsdjS12ozehdoIJ123DJOZJLDSCqsdeffdg123ER56SDFZedhWXojqshduzaohduihqsDAqsdq";
@@ -101,55 +109,25 @@ app.use((err, req, res, next) => {
 app.set("views", "./views");
 app.set("view engine", "ejs");
 
-// let frenchMovies = [
-//   { title: "Le fabuleux destin d'Amélie Poulain", year: 2001 },
-//   { title: "Buffet froid", year: 1979 },
-//   { title: "Le diner de cons", year: 1998 },
-//   { title: "De rouille et d'os", year: 2012 },
-// ];
-
-app.get("/movies", (req, res) => {
-  // res.send("Bientôt des films ici-même");
+app.get("/movies", async (req, res) => {
   const title = "Films français des 30 dernières années";
 
-  frenchMovies = [];
-  Movie.find((err, movies) => {
-    if (err) {
-      console.error("could not retrieve movies from DB", err);
-      res.sendStatus(500);
-    } else {
-      frenchMovies = movies;
-      res.render("movies", { movies: frenchMovies, title: title });
-    }
-  });
+  try {
+    const movies = await Movie.find();
+    res.render("movies", { movies: movies, title: title });
+  } catch (err) {
+    console.error("could not retrieve movies from DB", err);
+    res.sendStatus(500);
+  }
 });
 
 var urlencoded = bodyParser.urlencoded({ extended: false });
-
-// app.post('/movies', urlencoded, (req, res) => {
-//   const title = req.body.movietitle ;
-//   const year = req.body.movieyear  ;
-//   if(!title || !year){
-//     return res.status(400).send("Erreur : Titre et année requis !");
-//   }
-//   frenchMovies.push({ title, year });
-//   console.log("Film ajouté : ", title, year);
-//   res.redirect('/movies');
-//   // console.log("le titre :", req.body.movietitle);
-//   // console.log("l'année :", req.body.movieyear);
-// })
 
 app.post("/movies", upload.fields([]), (req, res) => {
   if (!req.body) {
     return res.sendStatus(500);
   } else {
     const formData = req.body;
-    // const title = req.body.movietitle;
-    // const year = req.body.movieyear;
-    // console.log(("formData :", formData));
-    // frenchMovies.push({ title, year });
-    //res.sendStatus(201);
-
     const title = req.body.movietitle;
     const year = req.body.movieyear;
     const myMovie = new Movie({ movieTitle: title, movieYear: year });
@@ -174,44 +152,78 @@ app.get("/movies/:id", (req, res) => {
   res.render("movie-details", { movieId: id });
 });
 
-app.get("/movie-deatils/:id", (req, res) => {
+app.get("/movie-details/:id", async (req, res) => {
   const id = req.params.id;
-  Movie.findById(id, (err, movie) => {
+  try {
+    const movie = await Movie.findById(id);
     console.log("movie ", movie);
-    res.render("/movie-details", { movieId: _id });
-  });
+
+    // Check if the request wants JSON (for API) or HTML (for browser)
+    const acceptHeader = req.headers.accept || "";
+    if (acceptHeader.includes("application/json")) {
+      return res.json({ movie: movie });
+    }
+
+    res.render("movie-details", { movieId: movie._id });
+  } catch (err) {
+    console.error("Error finding movie:", err);
+    res.status(404).send("Movie not found");
+  }
 });
 
-app.put("/movie-details/:id", urlencoded, (req, res) => {
+app.put("/movie-details/:id", upload.fields([]), async (req, res) => {
   const id = req.params.id;
   if (!req.body) {
-    return res.sendStatus(500);
+    return res.status(400).send("Request body is missing");
   }
-  console.log(
-    "movietitle: ",
-    req.body.movietitle,
-    "movieyear",
-    req.body.movieyear
-  );
-  Movie.findByIdAndUpdate(
-    id,
-    {
-      $set: { movieTitle: req.body.movietitle, movieYear: req.body.movieyear },
-    },
-    { new: true },
-    (err, movie) => {
-      if (err) {
-        console.log(err);
-        return res.send("Le film n'a pas pu être mis à jour");
-      } else {
-        res.redirect("/movies");
-      }
+
+  // Log detailed request information for debugging
+  console.log("Request headers:", req.headers);
+  console.log("Content-Type:", req.headers["content-type"]);
+  console.log("Request body:", req.body);
+  console.log("Request body type:", typeof req.body);
+  console.log("Request body keys:", Object.keys(req.body));
+
+  // Extract data from multipart/form-data request
+  const movieTitle = req.body.movietitle;
+  const movieYear = req.body.movieyear;
+
+  console.log("movietitle:", movieTitle, "movieyear:", movieYear);
+
+  // Validate inputs
+  if (!movieTitle && !movieYear) {
+    return res.status(400).send("Both movie title and year are missing");
+  }
+
+  // Build update object with only provided fields
+  const updateObj = {};
+  if (movieTitle) updateObj.movieTitle = movieTitle;
+  if (movieYear) updateObj.movieYear = parseInt(movieYear, 10) || movieYear;
+
+  try {
+    const movie = await Movie.findByIdAndUpdate(
+      id,
+      { $set: updateObj },
+      { new: true }
+    );
+
+    if (!movie) {
+      return res.status(404).send("Film non trouvé");
     }
-  );
+
+    res.json({
+      success: true,
+      message: "Film mis à jour avec succès",
+      movie: movie,
+    });
+    res.redirect('/movies');
+  } catch (err) {
+    console.log("Error updating movie:", err);
+    res.status(500).send("Le film n'a pas pu être mis à jour");
+  }
 });
 
 app.get("/", (req, res) => {
-  // res.send("Hello World!!!!");
   res.render("index");
 });
 
@@ -251,7 +263,6 @@ app.post("/login", urlencoded, (req, res) => {
   }
 });
 
-// Replace your existing /member-only route with this
 app.get("/member-only", (req, res) => {
   const authHeader = req.headers.authorization;
   console.log("Authorization header:", authHeader);
